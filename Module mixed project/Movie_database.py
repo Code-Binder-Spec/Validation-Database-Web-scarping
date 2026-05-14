7d439680f1d6351253fbc8dc91f780f7359f3da0
Code-Binder-Spec <adhimansoor.k@gmail.com>
2026-05-14 20:13:34 +0530

    Function cleaning tommorow

--- Module mixed project/Movie_database.py
+++ Module mixed project/Movie_database.py
@@ -9,10 +9,10 @@ class EnsuringData(BaseModel):
 
      @model_validator(mode='after')
      def Ensuring(self):
-               if self.name == "":
+               if self.name.strip() == "":
                      raise ValueError("Movie name cannot be empty")
-               if self.rating <= 0 :
-                     raise ValueError("Rating cant be less than or equal to zero")
+               if self.rating <= 0 or self.rating > 10:
+                     raise ValueError("Rating cant be less than or equal to zero and more than 10")
                if self.year > 2026 or self.year < 1878:
                      raise ValueError("Movie year cant be future or early before movie existed ...")
                return self
@@ -33,8 +33,8 @@ cur = conn.cursor()
 
 cur.execute("""
 
-            CREATE TABLE IF NOT EXISTs movies(
-                    movie_name TEXT,
+            CREATE TABLE IF NOT EXISTS movies(
+                    movie_name TEXT UNIQUE,
                     year INTEGER,
                     rating REAL
             )
@@ -51,8 +51,9 @@ def checking_row_num():
 while True:
                  try:
                                    options = int(input(" Please Choose an Option \n 1.Add movie \n 2.Show all movies \n 3.Show top movies \n 4.Update rating \n 5.Delete Movie \n :  "))
-                 except Exception as e :
-                                             print(f"Invalid input . Reason {e}")
+                 except :
+                             print("\n Sorry we couldnt recognise the input . Try again")
+                             continue
                  if options == 1:
                                         try:
                                                           movie_name = str(input("\n Enter movie name : ")).lower()
@@ -95,10 +96,17 @@ while True:
                                       try:
                                                                  checking_rows(rows=row)
                                                                  changing_movie = str(input("\n Enter the movie name [rating changing movie] : ")).lower()
-                                                                 new_rate = float(input(" Enter the new rating : "))
-                                                                 cur.execute("UPDATE movies SET rating = ? WHERE movie_name = ? ",(new_rate,changing_movie))
-                                                                 conn.commit()
-                                                                 print("\n Rating Updated")            
+                                                                 changing_movie_year = cur.execute("SELECT year FROM movies WHERE movie_name = ? ",(changing_movie,))
+                                                                 changing_movie_year = changing_movie_year.fetchone()
+                                                                 if changing_movie_year:
+                                                                                 cm_year = changing_movie_year[0]
+                                                                                 new_rate = float(input(" Enter the new rating : "))
+                                                                                 EnsuringData(name=changing_movie,year=cm_year,rating=new_rate)
+                                                                                 cur.execute("UPDATE movies SET rating = ? WHERE movie_name = ? ",(new_rate,changing_movie))
+                                                                                 conn.commit()
+                                                                                 print("\n Rating Updated")
+                                                                 else : 
+                                                                         print("\n The movie nae you provided doesnt exist")            
                                       
                                       except Exception as e :
                                                                    print(f"\n Showing Movies failed . Reason : {e}")               
@@ -107,22 +115,26 @@ while True:
                                       row = checking_row_num()
                                       try : 
                                                                  checking_rows(rows=row)
-                                                                 deleting_movie = str(input("\n Enter Movie name [Deleting movie] : "))        
+                                                                 deleting_movie = str(input("\n Enter Movie name [Deleting movie] : ")).lower()        
                                                                  cur.execute("DELETE FROM movies WHERE movie_name = ?",(deleting_movie,))    
                                                                  conn.commit()
-                                                                 print("\n Movie deleted ")
+                                                                 if cur.rowcount :
+                                                                               print("\n Movie deleted ")
+                                                                 else :
+                                                                               print("\n Movie not exist in database")
+                                                                         
                                       except Exception as e :
                                                                print(f"\n Showing Movies failed . Reason : {e}") 
 
                  else :
                           print("\n Option not exist")  
-                                
-                
-                 con_ex = str(input("\n Do you want to continue to menu : "))
+
+                 
+                 con_ex = str(input("\n Do you want to continue to menu ? : "))
                  if con_ex.lower() == "yes":
                          continue
                  elif con_ex.lower() == "no":
                          break
                  else :
                          print("\n Since you typed wrong response we are defaulting to quit all Your changes are saved No worries ✅")
-                 
+conn.close()
