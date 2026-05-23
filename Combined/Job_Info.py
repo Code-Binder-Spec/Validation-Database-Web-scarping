5bbe888cd2e6d6a34c22a4b90f750858d6629f8c
Code-Binder-Spec <adhimansoor.k@gmail.com>
2026-05-23 21:55:58 +0530

    Added job description

--- Combined/Job_Info.py
+++ Combined/Job_Info.py
@@ -9,6 +9,7 @@ class Ensuring_Data(BaseModel):
     Company : str
     Job_location : str 
     Job_posted_date : str
+    Job_description : str
 
     @model_validator(mode='after')
     def PreventingBugs(self):
@@ -30,31 +31,34 @@ def getting_response(urls):
         except Exception as e :
                      print(f"Url request failed . Reason : {e}")
 
-def extracting_everything(name,company,location):
-         for job in name :
-                    job_name = job.find("h2").next_sibling.strip()
-                    print(job_name)
-         company_name = company[0].find("br").next_sibling.strip()
-         print(company_name)
-         loc = location[0].find("a").text
-         return job_name,company_name,loc
+def extracting_everything(name,company,location,jobdes):
+         description = ""
+         h2 = name[0].find("h2") if name else None
+         job_name = h2.next_sibling.strip() if h2 else "missing"
+         for p in jobdes:
+                description = description + "\n" + p.text
+         br = company[0].find("br") if company else None
+         company_name = br.next_sibling.strip() if br else "missing"
+         a = location[0].find("a") if location else None
+         loc = a.text if a else "missing"
+         return job_name,company_name,loc,description
 
 def parsing_data(responses):
+        
          for response in responses :
              
              soup = BeautifulSoup(response,"html.parser")
              job_name = soup.select("div.job-description")
-             print(job_name)
+             job_description = soup.select("div.job-description p")
              company_name = soup.select("span.company-name")
-             print(company_name)
              job_loc = soup.select("span.listing-location")
-             print(job_loc)
-             job_date = soup.select("time")[0].text
-             print(job_date)
-             r_name,r_com,r_loc = extracting_everything(job_name,company_name,job_loc)
-
-             yield Ensuring_Data(Job_name=r_name,Company=r_com,Job_location=r_loc,Job_posted_date=job_date)
-
+             job_date = soup.select("time")
+             real_date = job_date[0].text if job_date else "missing"
+             r_name,r_com,r_loc,r_description = extracting_everything(job_name,company_name,job_loc,job_description)
+             try:
+                       yield Ensuring_Data(Job_name=r_name,Company=r_com,Job_location=r_loc,Job_posted_date=real_date,Job_description=r_description)
+             except ValueError as e:
+                       print(f"Data parsing failed . reason : {e}")
 urls = creating_url()
 response = getting_response(urls)
 parse = parsing_data(response)
