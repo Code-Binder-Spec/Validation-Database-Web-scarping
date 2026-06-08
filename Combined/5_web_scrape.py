3794050c80e7f0990e25e3a05d51886bfdb4dee1
Code-Binder-Spec <adhimansoor.k@gmail.com>
2026-06-08 21:36:36 +0530

    Big project loading now .. adding pydantic table of another web get the scraping data clearly then its done

--- Combined/5_web_scrape.py
+++ Combined/5_web_scrape.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from pydantic import BaseModel,field_validator
 import aiosqlite
 
-logging.basicConfig(filename="5web.log",level=logging.INFO)
+logging.basicConfig(filename=".gitignore/log/5web.log",level=logging.INFO)
 
 class flipkart_data_ensure(BaseModel):
         
@@ -16,7 +16,6 @@ class flipkart_data_ensure(BaseModel):
         @field_validator("features",mode="before")
         @classmethod
         def checking_feature(cls,v):
-                print(v)
                 if v.lower() == "description not found":
                         actual_v = None
                         return actual_v
@@ -46,20 +45,20 @@ async def getting_source(url,session):
        async with session.get(url,headers=headers) as response:
             return await response.text()
        
-def name_scraper(box):
+def flipkart_name_scraper(box):
      flag = True
      try :
                    name = box.find("div",class_="RG5Slk")
                    actual_name = checking_none(name)
      except Exception as e :
                flag = False 
-               logging.error(f"Name scraper failed due to error : {e}")
+               logging.error(f"Name scraper in flipkart failed due to error : {e}")
      if flag:
            return actual_name.text
      else :
            return None
      
-def price_scraper(box):
+def flipkart_price_scraper(box):
        flag = True 
        try:
                         price = box.find("div",class_="hZ3P6w DeU9vF")
@@ -67,14 +66,14 @@ def price_scraper(box):
        
        except Exception as e :
                flag = False
-               logging.error(f"Price scraping failed due to error . {e}")
+               logging.error(f"Price scraper in flipkart failed due to error . {e}")
        if flag :
                return actual_price.text
        else :
                return "price not found"
 
            
-def description_scraper(box):
+def flipkart_description_scraper(box):
      flag = True
      try:
                        description_box = box.find_all("ul",class_="HwRTzP")
@@ -85,21 +84,82 @@ def description_scraper(box):
                        full_des = ",".join(description_lis)
      except Exception as e :
              flag = False
-             logging.error(f"Name scraping failed due to error : {e}")
+             logging.error(f"description scraper in flipkart failed due to error : {e}")
 
      if flag :
              return full_des
      else :
             return "Description not found"
      
-       
+def flipkart_rating_scraper(box):
+        flag = True
+        try :
+                rating = box.find("div",class_="MKiFS6")
+                actual_rating = checking_none(rating)
+        except Exception as e :
+                flag = False
+                logging.error(f"Rating scraper in flipkart failed due to error : {e}")
+        if flag :
+                return actual_rating.text
+        else:
+                return "rating not found"
+        
+def flipkart_total_rating_scraper(box):
+        
+         flag = True
+         try :
+                 rating_box = box.find("span",class_="PvbNMB")
+                 actual_rate = checking_none(rating_box)
+                 lis = actual_rate.text.split()
+                 print(lis)
+         except Exception as e :
+                 flag = False
+                 logging.error(f"Total rating scraper in flipkart failed due to error : {e}")
+         if flag :
+                 return actual_rate.text
+         else :
+                 return "Total rating not found"
+
 def flipkart_scraper(box):
-     name = name_scraper(box)
-     description =  description_scraper(box)
-     price = price_scraper(box)
+     name = flipkart_name_scraper(box)
+     description =  flipkart_description_scraper(box)
+     price = flipkart_price_scraper(box)
+     rating = flipkart_rating_scraper(box)
+     total_rating = flipkart_total_rating_scraper(box)
      return name,description,price
-     
-     
+
+def amazon_name_scraper(block):
+          flag = True
+          try :
+                          name_block = block.find("a",class_="a-link-normal s-line-clamp-2 puis-line-clamp-3-for-col-4-and-8 s-link-style a-text-normal")
+                          real_name = name_block.find("span")
+                          actual_name = checking_none(real_name)
+          except Exception as e :
+                  flag = False
+                  logging.error(f"Name scraper in amazon failed due to error : {e}") 
+          if flag :
+                  return actual_name.text
+          else :
+                  return None
+          
+
+def amazon_price_scraper(block):
+        flag = True
+        try :
+                price = block.find("span",class_="a-price-whole")
+                actual_price = checking_none(price)
+        except Exception as e :
+                flag = False
+                logging.error(f"Price scraper in amazon failed due to error : {e}")
+        if flag :
+                return actual_price.text
+        else :
+                return "price not found"
+          
+def amazon_scraper(block):
+             name = amazon_name_scraper(block)
+             price = amazon_price_scraper(block)
+             return name,price
 
 async def passing_soup_block_flipkart(url,session,full_data):
      source = await getting_source(url,session)
@@ -112,30 +172,46 @@ async def passing_soup_block_flipkart(url,session,full_data):
           else :
                  full_data.append((name,description,price))
                  logging.info(f"Data fetched from url : {url} : succes ")
-      
+
+async def passing_soup_block_amazon(url,session,full_data_amazon):
+     
+        source = await getting_source(url,session)
+        soup = BeautifulSoup(source,"html.parser")
+        blocks = soup.find_all("div",role="listitem")
+        for block in blocks:
+                name,price = amazon_scraper(block)
+                if not name :
+                     continue
+                else :
+                        print(f"\n{name}\n{price}")
+                
+                 
 async def flipkart_sql_write(db,data):
-        await db.execute("INSERT INTO flipkart(name,features,price) VALUES (?,?,?)",(data.device,data.features,data.price))
+        await db.execute("INSERT OR IGNORE INTO flipkart(name,features,price) VALUES (?,?,?)",(data.device,data.features,data.price))
         await db.commit()
 
 
 async def main():
-    full_data = []
+    full_data_flipkart = []
+    full_data_amazon = []
     validated_data = []
-    async with aiosqlite.connect("5web.db") as db:
+    async with aiosqlite.connect(".gitignore/data/db/5web.db") as db:
                     await db.execute("""
                          CREATE TABLE IF NOT EXISTS flipkart(
                                      id INTEGER PRIMARY KEY,
                                      name TEXT,
                                      features TEXT,
-                                     price TEXT
+                                     price TEXT,
+                                     UNIQUE(name,features,price)
                                        ) 
                                            """)
                     await db.commit()
                     async with aiohttp.ClientSession() as session:
                                    await asyncio.gather(
-                                                    passing_soup_block_flipkart("https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=2",session,full_data)
+                                                    passing_soup_block_flipkart("https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=2",session,full_data_flipkart),
+                                                    passing_soup_block_amazon("https://www.amazon.in/s?k=phone&page=2&xpid=FcoF0gLBxLs4A&crid=18FZ9Q8TGYKTG&qid=1780907145&sprefix=phon%2Caps%2C389&ref=sr_pg_2",session,full_data_amazon)
                                                )
-                    for i in full_data:
+                    for i in full_data_flipkart:
                                ensure_data =  flipkart_data_ensure(device=i[0],features=i[1],price=i[2])
                                validated_data.append(ensure_data)
                     for data in validated_data:
